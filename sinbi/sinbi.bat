9793b1f11ac7736e4e2171a3b0573d499e4a646d
araato <araato@gmail.com>
2018-04-21 20:20:26 +0900

    신비아파트놀이 폴더 만듦

---
+++ sinbi/sinbi.bat
@@ -0,0 +1,5 @@
+@echo off
+:start
+python sinbi.py
+pause
+goto start 
--- sinbi/sinbi.py
+++ sinbi/sinbi.py
@@ -1,17 +1,17 @@
-
+﻿
 place = ['안방', '침대방', '서재', '놀이방']
 ghost = ['벽수귀', '흑진귀', '팬텀토르소', '모주귀',\
         '이드라', '무면귀', '마리오네트 퀸', '치돈귀',\
         '환마귀', '키클라스', '호문쿨루스', '망부화',\
         '골묘귀', '어둑시니', '불가살이', '라바나브',
-        '헤론', '객귀', '마고할망', '그슨새','지하국대적'\
-        '림 샤이코스', '이안', '시온', '요하임'\
+        '헤론', '객귀', '마고할망', '그슨새','지하국대적',\
+        '림 샤이코스', '이안', '시온', '요하임',\
         '혈안귀', '살음귀', '벨라', '백의귀',\
         '인큐버스', '케르베로스', '만티코어', '시두스',\
         '슬렌더맨', '바알제붑', '손각시', '네비로스', \
         '장도한', '홍콩할매', '강시', '외눈우산',\
         '구미호', '팔척귀신', '발록', '장산범',\
-		'망태할아버지', '조마구']
+	'망태할아버지', '조마구', '네코마타']
 
 import random
 import json
@@ -54,11 +54,12 @@ curGhost = chooseGhost()
 while curGhost in lastData['ghost'] :
 	curGhost = chooseGhost()
 
-print('{}에 {}'.format(curPlace, curGhost))
-
 lastData['place'] = curPlace
 lastData['ghost'] = lastData['ghost'][1:5]
 lastData['ghost'].append(curGhost)
 
 with open(datafile, 'w') as outfile:
     json.dump(lastData, outfile)
+
+print('')
+print('{}에 {}'.format(curPlace, curGhost))
